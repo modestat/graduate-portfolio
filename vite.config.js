fe65ec25da16ce62c2971e478c6ca01ef392d1b4
modesta <trakselytemodesta@gmail.com>
2025-09-30 12:55:00 +0200

    reahcnged base path in base at vite.config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/graduate-portfolio/",
   plugins: [react()],
+  base: "/" // tells Vite to use root for asset paths (important for GitHub Pages deployment)
 })
