ab27782c99a82f9c6e45a0ed761d337857a4b0aa
modesta <trakselytemodesta@gmail.com>
2026-01-16 13:10:52 +0100

    created notfound poage, and re-designed footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
 import './App.css';
-import {HashRouter, Routes, Route} from 'react-router-dom';
+import {HashRouter, Routes, Route, useLocation} from 'react-router-dom';
 import HomePage from './pages/Home';
 import AboutPage from './pages/About';
 import ProjectsPage from './pages/Projects';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
-import ProjectDetail from './components/ProjectDetail';
+import NotFound from './pages/NotFound';
+
+
+// to handle conditional logic for the footer render
+const ConditionalFooter = () => {
+  const location = useLocation(); 
+  const noFooterPaths = ['/'];    // which paths shoudl not shwo the footer
+  const showFooter = !noFooterPaths.includes(location.pathname);   // then also check if the current path is in the noFooterPaths array
+  return showFooter ? <Footer /> : null;  // only render footer is showFooter is true
+};
 
 function App() {
   return (
@@ -16,10 +25,9 @@ function App() {
           <Route path="/" element={<HomePage />} />
           <Route path="/about" element={<AboutPage />} />
           <Route path="/projects" element={<ProjectsPage />} /> 
-
-          { /*TODO: <Route path="*" element={<NotFound />} /> /* Catch-all for unmatched routes */}
+          <Route path="*" element={<NotFound />} /> 
         </Routes>
-        <Footer />
+        <ConditionalFooter />
       </HashRouter>
     </>
   )
