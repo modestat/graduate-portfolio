bb54c87911c158493213769cb5b84c7ae9a5ae76
modesta <trakselytemodesta@gmail.com>
2026-05-18 22:50:06 +0200

    "Add more aria-labels"

--- src/components/ProjectDetail.jsx
+++ src/components/ProjectDetail.jsx
@@ -46,13 +46,13 @@ const ProjectDetail = ({ project, imageNumber, onClose, onNextImage, onPrevImage
                className="gallery-image"
              />
              
-             <button className="carousel-btn prev" onClick={onPrevImage}>
+             <button className="carousel-btn prev" onClick={onPrevImage} aria-label="Previous image">
                <svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
                  <polyline points="15 18 9 12 15 6"></polyline>
                </svg>
              </button>
              
-             <button className="carousel-btn next" onClick={onNextImage}>
+             <button className="carousel-btn next" onClick={onNextImage} aria-label="Next image">
                <svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
                  <polyline points="9 18 15 12 9 6"></polyline>
                </svg>
